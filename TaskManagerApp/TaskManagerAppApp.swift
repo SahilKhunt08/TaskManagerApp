056bca3306a9caf5fc7658b98f5861d3aa46347d
Sahil K <skhunt125@gmail.com>
2024-05-30 19:49:46 -0500

    Adding Essential Files

---
+++ TaskManagerApp/TaskManagerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerAppApp.swift
+//  TaskManagerApp
+//
+//  Created by Sahil Khunt on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
