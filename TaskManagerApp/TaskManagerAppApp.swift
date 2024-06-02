edd18ad395562f2d5e6c763c8a2c137a85edf0bd
Sahil K <skhunt125@gmail.com>
2024-06-02 15:52:20 -0500

    Adding initial firebase code

--- TaskManagerApp/TaskManagerAppApp.swift
+++ TaskManagerApp/TaskManagerAppApp.swift
@@ -6,12 +6,32 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseAuth
+
+// Firebase code
+import FirebaseCore
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+      Auth.auth().useEmulator(withHost: "localhost" , port: 9099)
+    return true
+  }
+}
+
 
 @main
 struct TaskManagerAppApp: App {
+    
+    // Register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
-            LoginView()
+//            NavigationView {
+                LoginView()
+//            }
         }
     }
 }
